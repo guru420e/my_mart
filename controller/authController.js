dcfd3efb696f8cb439d6ffea581fd2bd646f8444
guru420e <vishes.11102001@gmail.com>
2025-04-25 02:27:25 +0530

    added user signup

--- controller/authController.js
+++ controller/authController.js
@@ -1,15 +1,21 @@
+import e from "express";
+import User from "../modals/userModal.js";
+
 // render the signup page
 function signupConroller(req, res) {
-  res.render('customer/auth/signup');
+  res.render("customer/auth/signup");
 }
-
 
 // handles the post request for signup
-function postSignupConroller(req, res) {
-  console.log(req.body);
-  res.send('Post request for signup');
+async function postSignupConroller(req, res) {
+  // need to add the validation for the email and password
+  const { email, confirmEmail, password, fullName: name, street, postalCode, city } = req.body;
+
+  const newUser = new User(email, password, name, street, postalCode, city);
+
+  await newUser.save();
+
+  res.redirect("/login");
 }
 
-
-
-export { signupConroller ,postSignupConroller};
+export { signupConroller, postSignupConroller };
---
+++ modals/userModal.js
@@ -0,0 +1,43 @@
+import { getDb } from "../data/database.js";
+import { saltRounds } from "../utils/constants.js";
+import bcrypt from "bcryptjs";
+
+class User {
+    constructor(
+        email,
+        password,
+        fullName,
+        street,
+        postalCode,
+        city,
+    ) {
+        this.email = email;
+        this.password = password;
+        this.name = fullName;
+        this.address = {
+            street: street,
+            postalCode: postalCode,
+            city: city,
+        };
+    }
+
+    async save() {
+        const db = getDb();
+
+        try {
+
+            const hashPassword = await bcrypt.hash(this.password, saltRounds);
+            const result = await db.collection("users").insertOne({
+                email: this.email,
+                password: hashPassword,
+                name: this.name,
+                address: this.address,
+            });
+            console.log(result);
+        } catch (err) {
+            console.log("Error registering User\n" + err);
+        }
+    }
+}
+
+export default User;
